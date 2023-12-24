795ae2f75baedaf6f8d23ea07722f6af11d1efc0
Totally Not HarroweD <46828061+NotHarroweD@users.noreply.github.com>
2023-12-24 18:45:31 -0500

    initial commit
    put all the things in

---
+++ ComfyUI-Harronode/backups/__init__.py
@@ -0,0 +1,11 @@
+from .harronode import Harronode
+
+NODE_CLASS_MAPPINGS = {
+    "Harronode": Harronode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Harronode": "Harronode - Prompt Builder"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
